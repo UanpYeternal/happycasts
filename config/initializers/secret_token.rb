d4e807af4bbfc1e991e623e524b9e26cf7a9a990
DerryCross <derrycross@gmail.com>
2013-09-25 21:38:43 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Happycasts::Application.config.secret_key_base = '0c84f7a8cd30f2abeb8077b2ff3040e8e91667afbb4d487206a729cb6b76bfcbdea294393a114079d0640871c2170e55a44607d1ccac3b9e5a7f0ebb19db80a0'
